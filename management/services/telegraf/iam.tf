7f67d503bdbe5c467fa02f80e1964320ab53f6de
Danny <danny.butroyd@gmail.com>
2018-01-24 21:39:33 +0000

    telegraf

---
+++ management/services/telegraf/iam.tf
@@ -0,0 +1,20 @@
+//module "aws_iam_role" {
+//  source = "../../../modules/local/aws-iam-role"
+//
+//  name = "influxdb"
+//}
+//
+//module "aws_iam_role_policy" {
+//  source = "../../../modules/local/aws-iam-role-policy"
+//
+//  name   = "cloudwatch_read_access"
+//  role   = "${module.aws_iam_role.aws_iam_role_name}"
+//  policy = "${file("lib/cloudwatch_iam.json")}"
+//}
+//
+//module "aws_iam_instance_profile" {
+//  source = "../../../modules/local/aws-iam-instance-profile"
+//
+//  name = "influxdb"
+//  role = "${module.aws_iam_role.aws_iam_role_name}"
+//}
---
+++ management/services/telegraf/security-groups.tf
@@ -0,0 +1,43 @@
+module "influxdb_security_group" {
+  source = "../../../modules/local/aws-security-group"
+
+  security_group_name         = "influxdb"
+  security_group_description  = "influxdb"
+  vpc_id                      = "${data.terraform_remote_state.infrastructure.vpc_id}"
+}
+
+module "security_group_rule_allow_ssh_from_world" {
+  source = "../../../modules/local/aws-security-group-rule"
+
+  rule_type         = "ingress"
+  protocol          = "tcp"
+  to_port           = "22"
+  from_port         = "22"
+  cidr_blocks       = "0.0.0.0/0"
+  security_group_id = "${module.influxdb_security_group.security_group_id}"
+
+}
+
+//module "security_group_rule_allow_3000_from_world" {
+//  source = "../../../modules/local/aws-security-group-rule"
+//
+//  rule_type         = "ingress"
+//  protocol          = "tcp"
+//  to_port           = "3000"
+//  from_port         = "3000"
+//  cidr_blocks       = "0.0.0.0/0"
+//  security_group_id = "${module.influxdb_security_group.security_group_id}"
+//
+//}
+
+module "security_group_rule_allow_outbound_all" {
+  source = "../../../modules/local/aws-security-group-rule"
+
+  rule_type         = "egress"
+  protocol          = "-1"
+  to_port           = 0
+  from_port         = 0
+  cidr_blocks       = "0.0.0.0/0"
+  security_group_id = "${module.influxdb_security_group.security_group_id}"
+
+}
